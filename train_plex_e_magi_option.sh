5e85647fdac089d56f05bc1f9d2555b9dcf266ef
jongyoon <john.jongyoon.kim@snu.ac.kr>
2024-11-26 09:45:08 +0900

    plex-e train script updated

--- train_plex_e_magi_option.sh
+++ train_plex_e_magi_option.sh
@@ -6,7 +6,6 @@ OUTPUT_DIR=$2;
 LANGUAGE=$3;
 JOB_ID=$4;
 MODEL_NAME=$5;
-alpha=${6:-1024};
 
 JOB_PORT=$((60000 + ${JOB_ID}))
 
@@ -20,11 +19,17 @@ MODEL_PATH=${MODEL_NAME}
 RUN_GPU_IDS=${CUDA_VISIBLE_DEVICES:-1}
 echo "RUNNING ON GPU: $RUN_GPU_IDS"
 unset CUDA_VISIBLE_DEVICES
+
+lora_merged_ckpt_path=/data/DeepSeek-Coder/experiments-magi-lora-merged/deepseek-ai/deepseek-coder-6.7b-base/ise-uiuc/Magicoder-OSS-Instruct-75K/$LANGUAGE
+largest_checkpoint=$(ls -d ${lora_merged_ckpt_path}/checkpoint-* 2>/dev/null | grep -oP 'checkpoint-\K[0-9]+' | sort -nr | head -n 1)
+
+params_num=202215383 # about 3% of the total number of deepseekcoder-6.7b (6740512768)
 deepspeed \
     --include localhost:${RUN_GPU_IDS} \
     --master_port ${JOB_PORT} \
-finetune/finetune_deepseekcoder_peft.py \
+finetune/finetune_deepseekcoder_lt_sft.py \
     --model_name_or_path $MODEL_PATH \
+    --full_ckpt_path $lora_merged_ckpt_path/checkpoint-${largest_checkpoint} \
     --data_path $DATA_PATH \
     --output_dir $OUTPUT_PATH \
     --num_train_epochs 2 \
@@ -42,10 +47,18 @@ finetune/finetune_deepseekcoder_peft.py \
     --gradient_checkpointing True \
     --report_to "wandb" \
     --deepspeed finetune/configs/ds_config_zero1_magi.json \
-    --peft_lora_r=$alpha --peft_lora_alpha=$alpha \
-    --bf16 True;
+    --bf16 True \
+    --ft_params_num ${params_num} \
+    --freeze_layer_norm \
+    --full_l1_reg 0.0 \
+    --sparse_l1_reg 0.0 \
+    --full_ft_max_epochs_per_iteration 2 \
+    --sparse_ft_max_epochs_per_iteration 2 \
+    --freeze_all --unfreeze_attn --unfreeze_ffn
+#    --mask_embed=True \
+
 
 #rm -rf ${OUTPUT_PATH}/**/global_step*;
 find "${OUTPUT_PATH}" -type d -name "global_step*" -exec rm -rf {} +
 
-PING $0 ${LANGUAGE} DONE;
+PING PLEX ${LANGUAGE} DONE;
